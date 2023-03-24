813fec0edba7889e458d30159cf08827f0977082
vyansidiyora2000 <90194323+vyansidiyora2000@users.noreply.github.com>
2023-03-24 17:01:39 +0530

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,27 @@
+import '../App.css';
+
+function ProductList({ product,addToCart }) {
+    return (
+        <div className='flex'>
+            {
+                product.map((productItem, productIndex) => {
+                    return (
+                        <div style={{ width: '33%', height: '33%' }}>
+                            <div className='product-item'>
+                                <img src={productItem.url} width="100%" style={{ width: '300px', height: '300px',borderRadius:'25px' }}/>
+                                <p>{productItem.name} | {productItem.category} </p>
+                                <p> {productItem.seller} </p>
+                                <p> Rs. {productItem.price} /-</p>
+                                <button className="btn btn-dark btn-block"
+                                    onClick={() => addToCart(productItem)}
+                                >Add To Cart</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default ProductList
